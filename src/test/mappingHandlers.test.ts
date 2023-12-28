430756c82ee346a5919813416093f3e01b01aa30
Thomas Nguyen <thomas.nguyen@darenft.com>
2023-12-28 16:10:01 +0700

    first UT

--- src/test/mappingHandlers.test.ts
+++ src/test/mappingHandlers.test.ts
@@ -1,4 +1,5 @@
 import { subqlTest } from "@subql/testing";
+import { Transfer } from "../types";
 
 /*
 // https://academy.subquery.network/build/testing.html
@@ -10,3 +11,18 @@ subqlTest(
   "handleEvent" //handler name
 );
 */
+
+subqlTest(
+  "handleLog test", // test name
+  30856764, // block height to process
+  [Transfer.create({
+    id: "0x8ab06e5820ae38371d0a14952751bd60dec3810dedcfb27582dcfc84d4a40601",
+    from: "0x9a489505a00cE272eAa5e07Dba6491314CaE3796",
+    to: "0x96629768e8b4D28AA3FfD4eBf29FF4FAe348Eb49",
+    value: BigInt(400000000000000000),
+    blockHeight: BigInt(30856764),
+    contractAddress: "0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd",
+  })], // dependent entities
+  [], // expected entities
+  "handleLog" //handler name
+);
